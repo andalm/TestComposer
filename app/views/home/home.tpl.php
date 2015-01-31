4e206092f94ec3deaaf5280f268dc62f4243df7d
andalm <andres.aldana.martinez@gmail.com>
2015-01-30 23:45:14 -0500

    Delete white spaces

--- app/views/home/home.tpl.php
+++ app/views/home/home.tpl.php
@@ -2,11 +2,11 @@
 <h4><?= $title ?></h4>
 
 <?php
-    echo App\Library\HtmlHelper::anchorTagList([
-        'Contactanos' => '/clase2PHP/contactos',
-        'Respuesta en JSon' => '/clase2PHP/contactos/json',
-        'Respuesta en XML' => '/clase2PHP/contactos/xml',
-        'Respuesta en String' => '/clase2PHP/contactos/string',
-        'Integracion Bootstrap' => '/clase2PHP/contactos/bootstrap',
-    ], 'p')
+  echo App\Library\HtmlHelper::anchorTagList([
+    'Contactanos' => '/clase2PHP/contactos',
+    'Respuesta en JSon' => '/clase2PHP/contactos/json',
+    'Respuesta en XML' => '/clase2PHP/contactos/xml',
+    'Respuesta en String' => '/clase2PHP/contactos/string',
+    'Integracion Bootstrap' => '/clase2PHP/contactos/bootstrap',
+  ], 'p')
 ?>
