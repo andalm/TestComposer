49e5392265eb068bf0537a524bb9773737f6c2d4
andalm <andres.aldana.martinez@gmail.com>
2015-05-01 22:44:27 -0500

    test modifications

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+/*require_once __DIR__.'/vendor/autoload.php';
 
 use App\Library\RequestUrl;
 use App\Library\Request;
@@ -9,5 +9,7 @@ use App\Library\DataBase;
 DataBase::initialize();
 $requestUrl = new RequestUrl($_GET['url']);
 $request = new Request($requestUrl);
-$request->execute();
+$request->execute();*/
+
+echo "Fuck";
 
